ab846b43bc059b13d3151652829eb652ac2af8f5
kheulak <kheulak@gmail.com>
2026-02-08 10:52:24 +0800

    Update WalletConnectionProvider.jsx

--- src/contexts/WalletConnectionProvider.jsx
+++ src/contexts/WalletConnectionProvider.jsx
@@ -3,16 +3,19 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 export const WalletConnectionProvider = ({ children }) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
     const network = WalletAdapterNetwork.Mainnet;
 
-    // You can also provide a custom RPC endpoint.
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    // --- CRITICAL CHANGE: USE YOUR CUSTOM RPC HERE ---
+    // The public "clusterApiUrl" is blocked on Vercel. 
+    // We use a custom endpoint to avoid 403 errors.
+    const endpoint = "https://mainnet.helius-rpc.com/?api-key=2bbecd4e-9112-4388-841b-925f16fd87c0"; 
+    // -------------------------------------------------
 
     const wallets = useMemo(
         () => [
